93fa1c222c2d6bfd89d7a5aa739f3f941a70e86e
btbl <vzalberto@msn.com>
2013-08-17 23:38:57 -0500

    Added inteligent displacement

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 	var angle = 0;
 	var margin = 0;
 	setInterval(function(){
-      angle+=3;
-      margin+=5;
-     $("#image").rotate(angle);
-     $("#image").css("marginLeft", margin+'px')
+	  bWidth = parseInt($(window).width());
+	  actualMargin = parseInt($("#image").css("marginLeft"));
+	  $("#info").text(actualMargin + ', ' + bWidth);
+	  if (actualMargin < bWidth - 200){
+	        angle+=3;
+	        margin+=5;
+	       $("#image").rotate(angle);
+	       $("#image").css("marginLeft", margin+'px')};
 	},10);
 });
